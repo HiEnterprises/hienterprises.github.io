0829b5572699aeab33a9606ff63945badbf92266
aarjay123 <76173037+aarjay123@users.noreply.github.com>
2025-08-04 16:16:19 +0100

    Began shifting custom tailwind styles into their own js files.

---
+++ assets/js/tailwind-config-harmony.js
@@ -0,0 +1,28 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'primary': '#a0c9ff',
+                'on-primary': '#00325a',
+                'primary-container': '#253141',
+                'on-primary-container': '#d2e4ff',
+                'secondary': '#bbc7db',
+                'on-secondary': '#253141',
+                'tertiary': '#d7bde4',
+                'on-tertiary': '#3b2947',
+                'tertiary-container': '#533f5f',
+                'on-tertiary-container': '#f3daff',
+                'background': '#1a1c1e',
+                'on-background': '#e3e2e6',
+                'surface': '#1a1c1e',
+                'on-surface': '#e3e2e6',
+                'outline': '#8d9199',
+                'surface-variant': '#43474e',
+                'on-surface-variant': '#c3c6cf',
+            },
+            fontFamily: {
+                sans: ['Cantarell', 'sans-serif'],
+            }
+        }
+    }
+}
---
+++ assets/js/tailwind-config-main.js
@@ -0,0 +1,9 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Cantarell', 'sans-serif'],
+            },
+        }
+    }
+}
